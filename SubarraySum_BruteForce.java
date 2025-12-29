76bb291b2131866c52433490cde69926f82bc80c
amarX007 <amarmajix7@gmail.com>
2025-12-29 10:40:20 +0530

    Code edited

--- SubarraySum_BruteForce.java
+++ SubarraySum_BruteForce.java
@@ -1,7 +1,10 @@
 package CodeSolving_DSA;
 
+/*Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.*/
 public class SubarraySum_BruteForce {
-    public int subarraysum(int nums[], int k) {
+
+    //Brute-force approach
+    public static int subarraysum(int[] nums, int k) {
         int count = 0;
         for (int i = 0; i < nums.length; i++) {
             int sum = 0;
@@ -15,13 +18,28 @@ public class SubarraySum_BruteForce {
         return count;
     }
 
+    //Better approach
+    /*public static int subarraySum(int[] arr) {
+        int n = arr.length;
+        int maxSum = arr[0];
+        int Csum = arr[0];
+
+        for (int j : arr) {
+            Csum = Math.max(j, Csum + j);
+            maxSum = Math.max(maxSum, Csum);
+        }
+        return maxSum;
+    }*/
+
     public static void main(String[] args) {
-        SubarraySum_BruteForce Sol = new SubarraySum_BruteForce();
-        int[] nums = {1, 2, 3};
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         int k = 2;
 
-        int finalResult = Sol.subarraysum(nums, k);
-        System.out.println(finalResult);
-    }
+        int BruteForce_res = subarraysum(nums, k);
+        System.out.println(BruteForce_res);
+
+        /*int Better_res = subarraySum(nums);
+        System.out.println(Better_res);*/
+        }
 }
 
